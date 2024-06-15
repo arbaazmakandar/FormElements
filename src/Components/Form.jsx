a1d4b16da2d8f9334482d86d4dcba12da6b00240
arbaazmakandar <arbaazmakandar@gmail.com>
2024-06-15 12:17:21 +0530

    Full Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,25 +2,38 @@ import React, { useState } from "react";
 
 const Form = () => {
   const [fullName, setFullName] = useState("");
+  const [isTrue, setIsTrue] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     const { firstName, lastName } = e.target.elements;
-    if (firstName.value !== "" && lastName.value !== "")
+    if (firstName.value !== "" && lastName.value !== "") {
+      setIsTrue(true);
       setFullName("Full Name: " + firstName.value + " " + lastName.value);
+    }
   };
   return (
     <>
       <form action="" onSubmit={handleSubmit}>
         <h2>Full Name Display</h2>
         <label htmlFor="firstName">First Name:</label>
-        <input type="text" name="firstName" required />
+        <input
+          type="text"
+          name="firstName"
+          required
+          onChange={() => setIsTrue(false)}
+        />
         <br />
         <label htmlFor="lastName">Last Name:</label>
-        <input type="text" name="lastName" required />
+        <input
+          type="text"
+          name="lastName"
+          required
+          onChange={() => setIsTrue(false)}
+        />
         <br />
         <button type="submit">Submit</button>
       </form>
-      <div>{fullName}</div>
+      {isTrue && <div>{fullName}</div>}
     </>
   );
 };
